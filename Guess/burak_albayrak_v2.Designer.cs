32e22d06015c6c9b23a8cc51174630dcd6222071
Burak Albayrak <albayrak1burak@gmail.com>
2022-09-03 03:55:19 +0300

    First project in university
    2017

---
+++ Guess/burak_albayrak_v2.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Gorsel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İPUCU:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(18, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SAYI GİR:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(52, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(272, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TAHMİN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(136, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 31);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label3.Location = new System.Drawing.Point(99, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(304, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rakamın yeri doğru ise +1, yeri yanlış ise -1 puan ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(292, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(258, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "SAYIYI GÖR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(424, 241);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sayı Tahmin Oyunu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Guess/burak_albayrak_v2.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gorsel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string sayi;
+        string birinci, ikinci, ucuncu, dorduncu;
+        int sayac = 1;
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String tahmin_et = textBox1.Text;
+
+            birinci_tahmin = tahmin_et.Substring(0, 1);
+            ikinci_tahmin = tahmin_et.Substring(1, 1);
+            ucuncu_tahmin = tahmin_et.Substring(2, 1);
+            dorduncu_tahmin = tahmin_et.Substring(3, 1);
+
+            if (birinci_tahmin == ikinci_tahmin)
+                MessageBox.Show("Lütfen Rakamları Farklı Sayı Giriniz");
+            else if (birinci_tahmin == ucuncu_tahmin)
+                MessageBox.Show("Lütfen Rakamları Farklı Sayı Giriniz"); 
+            else if (birinci_tahmin == dorduncu_tahmin)
+                MessageBox.Show("Lütfen Rakamları Farklı Sayı Giriniz");
+            else if (ikinci_tahmin == ucuncu_tahmin)
+                MessageBox.Show("Lütfen Rakamları Farklı Sayı Giriniz");
+            else if (ikinci_tahmin == dorduncu_tahmin)
+                MessageBox.Show("Lütfen Rakamları Farklı Sayı Giriniz");
+            if (ucuncu_tahmin == dorduncu_tahmin)
+                MessageBox.Show("Lütfen Rakamları Farklı Sayı Giriniz");
+        else
+            {
+                string a = sayi;
+                string b = tahmin_et;
+                int artiPuan = 0;
+                int eksiPuan = 0;
+
+                sayac++;
+
+                for(int k=0; k<4; k++)
+                {
+                    if (a.Substring(k, 1) == b.Substring(k, 1))
+                        artiPuan += 1;
+
+                    if ((a.IndexOf(b.Substring(k, 1)) > -1) && ((a.IndexOf(b.Substring(k, 1)) != k)))
+                        eksiPuan++;
+                }
+                if(artiPuan !=4)
+                {
+                    label3.Text = Convert.ToString("+" + artiPuan + " - " + eksiPuan);
+                }
+                else if (artiPuan == 4)
+                {
+                    label3.Text = "+4-0";
+                    MessageBox.Show(sayac + ". Denemede Sayıyı buldun. Tebrikler!");
+                    button2.Enabled = false;
+                    button1.Enabled = true;
+                    button3.Enabled = false;
+                }
+            }
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Visible = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string birinci_tahmin, ikinci_tahmin, ucuncu_tahmin, dorduncu_tahmin;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Burak ALBAYRAK İyi şanslar diler...");
+            Random rastgeleSayi = new Random();
+            button2.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            label4.Visible = false;
+            textBox1.Enabled = true;
+
+            sayac = 0;
+           
+            for (int k = 0; k < 500; k++)
+            {
+                sayi = rastgeleSayi.Next(1023, 9876).ToString();
+                birinci = sayi.Substring(0, 1);
+                ikinci = sayi.Substring(1, 1);
+                ucuncu = sayi.Substring(2, 1);
+                dorduncu = sayi.Substring(3, 1);
+            }
+            if (birinci != ikinci && birinci != ucuncu && birinci != dorduncu && ikinci != ucuncu && ikinci != dorduncu && ucuncu != dorduncu)
+            {
+                label4.Text = sayi;
+            }
+            else
+                for (int k = 0; k < 500; k++)
+                {
+                    sayi = rastgeleSayi.Next(1023, 9876).ToString();
+                    birinci = sayi.Substring(0, 1);
+                    ikinci = sayi.Substring(1, 1);
+                    ucuncu = sayi.Substring(2, 1);
+                    dorduncu = sayi.Substring(3, 1);
+                }
+            if (birinci != ikinci && birinci != ucuncu && birinci != dorduncu && ikinci != ucuncu && ikinci != dorduncu && ucuncu != dorduncu)
+            {
+                label4.Text = sayi;
+            }
+            else
+                for (int k = 0; k < 500; k++)
+                {
+                    sayi = rastgeleSayi.Next(1023, 9876).ToString();
+                    birinci = sayi.Substring(0, 1);
+                    ikinci = sayi.Substring(1, 1);
+                    ucuncu = sayi.Substring(2, 1);
+                    dorduncu = sayi.Substring(3, 1);
+                }
+            if (birinci != ikinci && birinci != ucuncu && birinci != dorduncu && ikinci != ucuncu && ikinci != dorduncu && ucuncu != dorduncu)
+            {
+                label4.Text = sayi;
+            }
+            else
+                for (int k = 0; k < 500; k++)
+                {
+                    sayi = rastgeleSayi.Next(1023, 9876).ToString();
+                    birinci = sayi.Substring(0, 1);
+                    ikinci = sayi.Substring(1, 1);
+                    ucuncu = sayi.Substring(2, 1);
+                    dorduncu = sayi.Substring(3, 1);
+                }
+        }
+    }
+}
